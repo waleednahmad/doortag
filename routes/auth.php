9f6b003a0f98f3637d42f3473e925128175c535c
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-06-27 20:38:57 +0300

    AHmad Alsakhen 27-6-2025 8:38 PM

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,11 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    // Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::get('register', function () {
+        // return to login route
+        return redirect()->route('login');
+    })->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
