3e4aef49d09592377e6cf03e8d4322da48781ddc
Ahmad Alsakhen <73029268+Al-sakhen@users.noreply.github.com>
2025-07-20 20:44:40 +0300

    Ahmad Alsakhen 20-7-2025 8:44 PM

--- app/Livewire/Shipping/Index.php
+++ app/Livewire/Shipping/Index.php
@@ -43,7 +43,7 @@ class Index extends Component
             'insuranceAmount' => '12.15',
             'declaredValue' => '100',
             // ---- Optional for api request
-            'ounces' => '', // Ounces are not used in the API
+            'ounces' => 0, // Ounces are not used in the API
         ]
     ];
 
@@ -115,7 +115,7 @@ class Index extends Component
             'pieces.*.height' => 'required|numeric|min:0.01',
             'pieces.*.insuranceAmount' => 'required|numeric|min:0',
             'pieces.*.declaredValue' => 'required|numeric|min:0',
-            'pieces.*.ounces' => 'nullable|integer|min:0|max:15', // Ounces are not used in the API, but we keep it for UI consistency
+            'pieces.*.ounces' => 'required|integer|min:0|max:15', // Ounces are not used in the API, but we keep it for UI consistency
         ]);
 
         try {
